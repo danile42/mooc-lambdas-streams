985d51657de318d1b08abd19608fcaa69548f717
daniel <dw@suljee.de>
2015-07-19 12:09:29 +0200

    Solve exercise 1 using standard collector.

--- lesson1/Lesson1.java
+++ lesson1/Lesson1.java
@@ -10,6 +10,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * @author Speakjava (simon.ritter@oracle.com)
@@ -45,10 +49,17 @@ public class Lesson1 {
    * of Strings provided.
    */
   private void exercise1() {
+    exercise1d();
+  }
+
+  private void exercise1d() {
     List<String> list = Arrays.asList(
         "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
 
-    /* YOUR CODE HERE */
+    String firstLetters = list.stream()
+                              .map(s -> String.valueOf(s.charAt(0)))
+                              .collect(joining(""));
+    System.out.println(firstLetters);
   }
 
   /**
